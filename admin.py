211d46fd4d6c8c93dafc978682febe1ba0559f68
Bipolar-Bear-13-cards <agapov.daniil@inbox.ru>
2022-11-23 19:30:21 +0300

    win

--- admin.py
+++ admin.py
@@ -30,6 +30,7 @@ class Widget(Qt.QWidget):
 		self.table.setHorizontalHeaderLabels(["Номер участка", "Фамилия владельца", "E-mail влодельца"," "," "," "," "," "])
 		for i in range(1):
 			self.sqr.append(Qt.QCheckBox())
+			self.sqr[i].setAlignment()
 			self.table.setCellWidget(i,0,self.sqr[i])
 			self.btn3.append(Qt.QPushButton())
 			self.table.setCellWidget(i, 4, self.btn3[i])
