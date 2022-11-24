784b914593dcae56cb696a39bef90b185702d0f6
Bipolar-Bear-13-cards <agapov.daniil@inbox.ru>
2022-11-24 08:18:24 +0300

    astra

---
+++ reg1.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+stty -F /dev/ttyUSB0 cs8 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts raw
+	while read LINE
+	do
+	echo $LINE
+	exit
+	done < /dev/ttyUSB0
+
