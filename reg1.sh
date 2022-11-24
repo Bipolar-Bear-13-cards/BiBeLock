292f072e0dbf8e77e28308481642a32c3153c055
Bipolar-Bear-13-cards <agapov.daniil@inbox.ru>
2022-11-24 08:40:06 +0300

    astra

--- reg1.sh
+++ reg1.sh
@@ -2,7 +2,7 @@
 stty -F /dev/ttyUSB0 cs8 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts raw
 	while read LINE
 	do
-	echo $LINE
+	sudo python3 reg1.py $LINE
 	exit
 	done < /dev/ttyUSB0
 
