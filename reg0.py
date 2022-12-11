2c0f6031ba0f891c909915cf89f2116e70767e7d
Bipolar-Bear-13-cards <agapov.daniil@inbox.ru>
2022-12-11 23:43:55 +0300

    VS

--- reg0.py
+++ reg0.py
@@ -7,13 +7,24 @@ import sys
 
 class Widget(Qt.QWidget):
     def __init__(self,table):
+        f = open("test","w+")
+        f.write("0")
+        f.close()
         super().__init__()
+        timer = QtCore.QTimer(self, timeout=self.Cuff, interval=1000)
+        timer.start()
         layout1 = Qt.QVBoxLayout(self)
         txt1=Qt.QLabel("Приложите карту к считывателю")
         txt1.setAlignment(QtCore.Qt.AlignCenter)
         layout1.addWidget(txt1)
         os.system("sudo ./reg1.sh") 
         self.close()
+        self.Cuff()
+
+    def Cuff():
+        f = open("test","r")
+        if(f.read!=0):
+            self.close()
 
 
 
