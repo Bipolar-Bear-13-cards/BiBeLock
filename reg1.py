49dfce04a8fdf7d7679c0231501bddf2cdcac425
Bipolar-Bear-13-cards <agapov.daniil@inbox.ru>
2022-12-12 11:00:26 +0300

    VS

--- reg1.py
+++ reg1.py
@@ -1,8 +1,59 @@
 import os
+from PyQt5 import QtWidgets, QtCore, Qt
+from PyQt5.QtCore import QSize
 import sys
 
 
 
+
+
+class Widget(Qt.QWidget):
+	pole1=[]
+	pole2=[]
+	pole3=[]
+	def __init__(self,table):
+		super().__init__()
+		self.setWindowTitle("���������� ������ ������������")
+		layout1 = Qt.QVBoxLayout(self)
+		txt1=Qt.QLabel("�������:")
+		self.pole1=Qt.QLineEdit()
+		txt1.setAlignment(QtCore.Qt.AlignLeft)
+		self.pole1.setAlignment(QtCore.Qt.AlignLeft) 
+		txt2=Qt.QLabel("���:") 
+		self.pole2=Qt.QLineEdit() 
+		txt2.setAlignment(QtCore.Qt.AlignLeft) 
+		self.pole2.setAlignment(QtCore.Qt.AlignLeft)
+		txt3=Qt.QLabel("e-mail:") 
+		self.pole3=Qt.QLineEdit() 
+		txt3.setAlignment(QtCore.Qt.AlignLeft) 
+		self.pole3.setAlignment(QtCore.Qt.AlignLeft)
+		layout1.addWidget(txt1)
+		layout1.addWidget(self.pole1)
+		layout1.addWidget(txt2)
+		layout1.addWidget(self.pole2)
+		layout1.addWidget(txt3)
+		layout1.addWidget(self.pole3)
+		self.show()
+		sprbtn1=Qt.QPushButton("��")
+		sprbtn1.clicked.connect(self.okk)
+		sprbtn2=Qt.QPushButton("������")
+		sprbtn2.clicked.connect(self.cancel)
+		layout1.addWidget(sprbtn1)
+		layout1.addWidget(sprbtn2)
+
+	def okk(self):
+		#здесь должна быть запись пользователя в бд. ID находится в sys.argv[1]
+		f=open("test","w+")
+		f.write("no")
+		f.close
+
+	def cancel(self):
+		self.close()
+
+
 f=open("test","w+")
-f.write(sys.argv[1])
+f.write("ye")
 f.close()
+app1 = Qt.QApplication([])
+w1 = Widget(Qt.QTableWidget())
+sys.exit(app1.exec_())
