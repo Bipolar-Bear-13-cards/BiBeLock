3b57c4e953e966aaffda7bec852417e15bcd42ba
Bipolar-Bear-13-cards <agapov.daniil@inbox.ru>
2022-12-14 02:22:44 +0300

    VS

--- check_pass20.py
+++ check_pass20.py
@@ -1,10 +1,23 @@
 import sys
 import sqlite3
+import pyotp
 
 
-
-if sys.argv[1][0:21]=="4078740140|1111111111":
-	print(1)
+connection = sqlite3.connect('users.db')
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS Users
+                    (UID TEXT, sname TEXT, name TEXT, mail TEXT, PIN TEXT, key TEXT)''')
+UID=sys.argv[1][:sys.argv[1].index('|')]
+print(UID)
+cursor.execute("SELECT PIN FROM Users WHERE UID=?", (UID,))
+ifrf=cursor.fetchall()[0][0]
+if ifrf:
+	cursor.execute("SELECT key FROM Users WHERE UID=?", (UID,))
+	tapt=pyotp.TOTP(cursor.fetchall()[0][0])
+	if sys.argv[1][sys.argv[1].index('|')+1:] == ifrf+tapt.now():
+		print(1)
+	else:
+		print(0)
 else:
 	print(0)
 
