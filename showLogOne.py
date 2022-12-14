eefb4ffc9cde194c065f757d2828eb4024572efc
Bipolar-Bear-13-cards <agapov.daniil@inbox.ru>
2022-12-15 01:08:49 +0300

    VS151

--- showLogOne.py
+++ showLogOne.py
@@ -16,21 +16,7 @@ class Widget(Qt.QWidget):
 		timer.start()
 		self.setWindowTitle("информация по кодам доступа пользователя с id"+sys.argv[1])
 		connection = sqlite3.connect('users.db')
-		cursor = connection.cursor()
-		cursor.execute('''CREATE TABLE IF NOT EXISTS events
-                    (UID TEXT, dt TEXT, event TEXT, sost TEXT)''')
-		cursor.execute("SELECT * FROM Users WHERE UID=?",(sys.argv[1],))
-		self.events=cursor.fetchall()
-		connection.close()
 		self.table = table
-		self.table.setRowCount(len(self.events))
-		self.table.setColumnCount(2)
-		self.table.setHorizontalHeaderLabels(["Время события","Тип события"])
-		i=0
-		for event in self.events:
-			self.table.setItem(i, 1, QTableWidgetItem(event[1]))
-			self.table.setItem(i, 2, QTableWidgetItem(event[2]))
-			i += 1
 		layout = Qt.QVBoxLayout(self)
 		sprbtn1=Qt.QPushButton("сменить учётные данные")
 		#sprbtn1.clicked.connect(self.deleteAll)
@@ -42,25 +28,21 @@ class Widget(Qt.QWidget):
 		self.show()
 
 	def Cuff(self):
-		f=open(sys.argv[1]+"log")
-		inlines=''
-		f1=open(sys.argv[1]+"logCoutP")
-		n=int(f1.read())
-		f2=open(sys.argv[1]+"logCout")
+		cursor = connection.cursor()
+		cursor.execute('''CREATE TABLE IF NOT EXISTS events
+                    (UID TEXT, dt TEXT, event TEXT, sost TEXT)''')
+		cursor.execute("SELECT * FROM Users WHERE UID=?",(sys.argv[1],))
+		self.events=cursor.fetchall()
+		connection.close()
+		self.table.setRowCount(len(self.events))
+		self.table.setColumnCount(2)
+		self.table.setHorizontalHeaderLabels(["Время события","Тип события"])
 		i=0
-		for line in f:
-			if i<n:
-				inlines=(line+"\n"+inlines)
-			else:
-				inlines=("(новое)"+line+"\n"+inlines)
-			i+=1
-		if n+int(f2.read()) == 0:
-			self.txt.setText("       Сообщений нет       ")
-		else:
-			self.txt.setText(inlines[:-1])
-		f.close()
-		f1.close()
-		f2.close()
+		for event in self.events:
+			self.table.setItem(i, 1, QTableWidgetItem(event[1]))
+			self.table.setItem(i, 2, QTableWidgetItem(event[2]))
+			i += 1
+
 
 	def Pomet(self):
 		f1=open(sys.argv[1]+"logCout")
