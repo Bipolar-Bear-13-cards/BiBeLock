354ef932f860063aa745ad8d12615dc6e681d1ee
Bipolar-Bear-13-cards <agapov.daniil@inbox.ru>
2022-12-14 19:12:57 +0300

    VS

--- check_pass20.py
+++ check_pass20.py
@@ -1,6 +1,7 @@
 import sys
 import sqlite3
 import pyotp
+import datetime
 
 
 #f=open("potom udalu.txt","w+")
@@ -14,6 +15,14 @@ UID=sys.argv[1][:sys.argv[1].index('|')]
 cursor.execute("SELECT PIN FROM Users WHERE UID=?", (UID,))
 #f.write(ifrf+"\n")
 ifrf=cursor.fetchall()
+connection1 = sqlite3.connect('events.db')
+cursor1 = connection.cursor()
+cursor1.execute('''CREATE TABLE IF NOT EXISTS events
+                    (UID TEXT, dt TEXT, event TEXT)''')
+
+
+
+
 if ifrf:
 	PIN=ifrf[0][0]
 	cursor.execute("SELECT key FROM Users WHERE UID=?", (UID,))
@@ -25,10 +34,13 @@ if ifrf:
 	if sys.argv[1][sys.argv[1].index('|')+1:-1] == PIN+str(tapt.now()):
 		#f.write("совпалдение!!!")
 		print(1)
+		tekevent=[(UID,str(datetime.datetime.now())[0:16],"введены верные учётные данные")]
 	else:
 		print(0)
+		tekevent=[(UID,str(datetime.datetime.now())[0:16],"введены неверные постоянный и/или временный коды")]
 else:
 	print(0)
+	tekevent=[(UID,str(datetime.datetime.now())[0:16],"считана отсутсвующая в базе данных метка")]
 #f.close()
 connection.close()
 
