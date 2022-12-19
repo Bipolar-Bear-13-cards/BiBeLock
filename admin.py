bd453d4328939220ff557121d8b02213a69872c0
Bipolar-Bear-13-cards <agapov.daniil@inbox.ru>
2022-12-19 20:48:44 +0300

    vs 192048

--- admin.py
+++ admin.py
@@ -98,6 +98,7 @@ class Widget(Qt.QWidget):
 			if sq.isChecked():
 				cursor.execute("DELETE FROM Users WHERE UID = ?",(self.allusr[self.sqr.index(sq)][0],))
 				self.sqr.pop()
+		connection.commit()
 		cursor.execute("SELECT * FROM Users")
 		self.allusr=cursor.fetchall()
 		self.table.setRowCount(len(self.allusr))
